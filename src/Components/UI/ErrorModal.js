23ca283f3eb41ec2e64d241c793dd36fc5a30f40
Pann Sengpanich <panncraftyo@gmail.com>
2023-05-04 23:13:34 +0700

    create modal

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
@@ -6,18 +8,39 @@ function ErrorModal(props) {
   const onCloseErrorModal = () => {
     props.closeErrorModal();
   };
+  function Backdrop(props) {
+    return <div className={classes.backdrop} onClick={props.onConfirm}></div>;
+  }
+  function Modal(props) {
+    return (
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    );
+  }
   return (
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={classes.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={classes.actions}>
-        <Button onClick={onCloseErrorModal}>Okay</Button>
-      </footer>
-    </Card>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={onCloseErrorModal}></Backdrop>,
+        document.querySelector("#backdrop"),
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onConfirm={onCloseErrorModal}
+        ></Modal>,
+        document.querySelector("#modal"),
+      )}
+    </React.Fragment>
   );
 }
 export default ErrorModal;
